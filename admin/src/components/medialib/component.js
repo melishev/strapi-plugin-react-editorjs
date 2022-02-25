70aa649bbc5b2693d3246f863ba13e6a911e123a
Jason Skipper <jasonskipper@gmail.com>
2022-02-25 17:37:38 -0600

    chore: removed several console.log messages that are now not needed

--- admin/src/components/medialib/component.js
+++ admin/src/components/medialib/component.js
@@ -11,7 +11,6 @@ const MediaLibComponent = ({isOpen, onChange, onToggle}) => {
 
   const handleInputChange = data => {
     if (data) {
-      console.log(data);
       setData(data);
     }
   };
@@ -22,7 +21,6 @@ const MediaLibComponent = ({isOpen, onChange, onToggle}) => {
       url: prefixFileUrlWithBackendUrl(f.url),
       mime: f.mime,
     }));
-    console.log('handleSelectAssets');
     onChange(formattedFiles);
   };
 
