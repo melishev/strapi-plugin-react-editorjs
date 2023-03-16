f48340684e29021b1fac81c01e1a9309ac69a9a5
Tong Sheng Ng <tongsheng.ng@stashaway.com>
2023-03-17 01:31:29 +0800

    add custom button tool

--- admin/src/components/Wysiwyg/wrapper.js
+++ admin/src/components/Wysiwyg/wrapper.js
@@ -1,76 +1,88 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Box } from "@strapi/design-system/Box";
 
 const Wrapper = styled(Box)`
-    @media (min-width: 651px) {
-        .codex-editor--narrow .codex-editor__redactor {
-            margin-right: 0;
-        }
-    }    
-    .codex-editor {
-        padding: 16px;
-        font-size: 1rem;
+  @media (min-width: 651px) {
+    .codex-editor--narrow .codex-editor__redactor {
+      margin-right: 0;
     }
-    *:focus-visible {
-        outline: none;
+  }
+  .codex-editor {
+    padding: 16px;
+    font-size: 1rem;
+  }
+  *:focus-visible {
+    outline: none;
+  }
+  h1 {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+  h2 {
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+  h3 {
+    font-size: 1.17em;
+    font-weight: bold;
+  }
+  h4 {
+    font-size: 1em;
+    font-weight: bold;
+  }
+  h5 {
+    font-size: 0.83em;
+    font-weight: bold;
+  }
+  h6 {
+    font-size: 0.67em;
+    font-weight: bold;
+  }
+  label {
+    display: block;
+    margin-bottom: 1rem;
+  }
+  &.bordered {
+    .editorWrapper {
+      border-color: red;
     }
-    h1 {
-        font-size: 2.5rem;
-        font-weight: bold;
+  }
+  > div + p {
+    width: 100%;
+    padding-top: 12px;
+    font-size: 1.2rem;
+    line-height: normal;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-bottom: -9px;
+  }
+  div,
+  pre,
+  code {
+    ::-webkit-scrollbar {
+      height: 5px;
+      width: 5px;
+      cursor: default;
     }
-    h2 {
-        font-size: 1.5em;
-        font-weight: bold;
-    }
-    h3 {
-        font-size: 1.17em;
-        font-weight: bold;
-    }
-    h4 {
-        font-size: 1em;
-        font-weight: bold;
-    }
-    h5 {
-        font-size: .83em;
-        font-weight: bold;
-    }
-    h6 {
-        font-size: .67em;
-        font-weight: bold;
-    }
-    label {
-      display: block;
-      margin-bottom: 1rem;
-    }
-    &.bordered {
-      .editorWrapper {
-        border-color: red;
-      }
-    }
-    > div + p {
-      width: 100%;
-      padding-top: 12px;
-      font-size: 1.2rem;
-      line-height: normal;
-      white-space: nowrap;
-      overflow: hidden;
-      text-overflow: ellipsis;
-      margin-bottom: -9px;
-    }
-    div,
-    pre,
-    code {
-      ::-webkit-scrollbar {
-        height: 5px;
-        width: 5px;
-        cursor: default;
-      }
-    }
-    .cdx-input.image-tool__caption {
-      font-size: .9rem;
-      color: #444;
-      line-height: 1.5;
-    }
+  }
+  .cdx-input.image-tool__caption {
+    font-size: 0.9rem;
+    color: #444;
+    line-height: 1.5;
+  }
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    padding: 1rem;
+    background-color: #f5f5f5;
+    border-radius: 0.25rem;
+  }
+
+  .button-container label {
+    margin-bottom: 0;
+  }
 `;
 
 export default Wrapper;
--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -1,18 +1,68 @@
-import PluginId from '../pluginId'
+import PluginId from "../pluginId";
+import Embed from "@editorjs/embed";
+import Table from "@editorjs/table";
+import List from "@editorjs/list";
+import LinkTool from "@editorjs/link";
+import Header from "@editorjs/header";
+import Quote from "@editorjs/quote";
+import Marker from "@editorjs/marker";
+import CheckList from "@editorjs/checklist";
+import Delimiter from "@editorjs/delimiter";
 
-import Embed from '@editorjs/embed'
-import Table from '@editorjs/table'
-import List from '@editorjs/list'
-import Warning from '@editorjs/warning'
-import Code from '@editorjs/code'
-import LinkTool from '@editorjs/link'
-import Raw from '@editorjs/raw'
-import Header from '@editorjs/header'
-import Quote from '@editorjs/quote'
-import Marker from '@editorjs/marker'
-import CheckList from '@editorjs/checklist'
-import Delimiter from '@editorjs/delimiter'
-import InlineCode from '@editorjs/inline-code'
+class Button {
+  static get toolbox() {
+    return {
+      title: "Button",
+      icon: '<svg width="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M320 96a32 32 0 1 1 -64 0 32 32 0 1 1 64 0zm21.1 80C367 158.8 384 129.4 384 96c0-53-43-96-96-96s-96 43-96 96c0 33.4 17 62.8 42.9 80H224c-17.7 0-32 14.3-32 32s14.3 32 32 32h32V448H208c-53 0-96-43-96-96v-6.1l7 7c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9L97 263c-9.4-9.4-24.6-9.4-33.9 0L7 319c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l7-7V352c0 88.4 71.6 160 160 160h80 80c88.4 0 160-71.6 160-160v-6.1l7 7c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-56-56c-9.4-9.4-24.6-9.4-33.9 0l-56 56c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l7-7V352c0 53-43 96-96 96H320V240h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H341.1z"/></svg>',
+    };
+  }
+
+  constructor({ data }) {
+    this.data = data;
+  }
+
+  render() {
+    this.wrapper = document.createElement("div");
+
+    const href = this.data && this.data.href ? this.data.href : "";
+    const label = this.data && this.data.label ? this.data.label : "";
+    const openInNewTab = this.data && this.data.openInNewTab ? true : false;
+
+    const checkboxId = `open-in-new-tab-${Math.random()}`;
+
+    const html = `
+      <div class="button-container">
+        <h4>Button</h4>
+        <input type="text" class="cdx-input button-label" placeholder="ex: Learn more or Get started" value="${label}">
+        <input type="text" class="cdx-input button-href" placeholder="ex: /simple or https://stashaway.sg/simple" value="${href}">
+        <label for="${checkboxId}">
+          <input type="checkbox" class="button-open-in-new-tab" id="${checkboxId}" ${
+      openInNewTab ? "checked" : ""
+    }>
+          Open in new tab
+        </label>
+      </div>
+    `;
+
+    this.wrapper.innerHTML = html;
+
+    return this.wrapper;
+  }
+
+  save(blockContent) {
+    const hrefInput = blockContent.querySelector(".button-href");
+    const labelInput = blockContent.querySelector(".button-label");
+    const openInNewTabCheckbox = blockContent.querySelector(
+      ".button-open-in-new-tab"
+    );
+
+    return {
+      href: hrefInput.value,
+      label: labelInput.value,
+      openInNewTab: openInNewTabCheckbox.checked ? true : false,
+    };
+  }
+}
 
 const customTools = {
   embed: Embed,
@@ -20,28 +70,18 @@ const customTools = {
     class: Table,
     inlineToolbar: true,
   },
+  button: {
+    class: Button,
+  },
   list: {
     class: List,
     inlineToolbar: true,
   },
-  warning: {
-    class: Warning,
-    inlineToolbar: true,
-    config: {
-      titlePlaceholder: 'Title',
-      messagePlaceholder: 'Message',
-    },
-  },
-  code: Code,
   LinkTool: {
     class: LinkTool,
     config: {
       endpoint: `/api/${PluginId}/link`,
     },
-  },
-  raw: {
-    class: Raw,
-    inlineToolbar: true,
   },
   header: {
     class: Header,
@@ -51,8 +91,8 @@ const customTools = {
     class: Quote,
     inlineToolbar: true,
     config: {
-      quotePlaceholder: 'Quote',
-      captionPlaceholder: 'Quote`s author',
+      quotePlaceholder: "Quote",
+      captionPlaceholder: "Quote`s author",
     },
   },
   marker: {
@@ -65,6 +105,6 @@ const customTools = {
   },
   delimiter: Delimiter,
   inlineCode: InlineCode,
-}
+};
 
-export default customTools
+export default customTools;
